947a0b0d65d1cc69b24994587d3ddecdc6bb0ee6
ParkSungGyu1 <steven0507@naver.com>
2023-05-24 10:48:31 +0900

    Fix : swagger description 수정

--- src/main/java/com/project/loupang/oauth/controller/KakaoOauthController.java
+++ src/main/java/com/project/loupang/oauth/controller/KakaoOauthController.java
@@ -3,6 +3,9 @@ package com.project.loupang.oauth.controller;
 import com.project.loupang.oauth.KakaoLoginParams;
 import com.project.loupang.oauth.jwt.AuthTokens;
 import com.project.loupang.oauth.service.OAuthLoginService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +20,10 @@ public class KakaoOauthController {
 
     private final OAuthLoginService oAuthLoginService;
 
+    @Operation(summary = "카카오 로그인 코드 return", description = "카카오 로그인 코드 return", tags = { "KakaoOauthController" })
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK")
+    })
     @GetMapping("/v1/api/oauth")
     public Map<String,String> oauthKakao(@RequestParam String code) {
         HashMap<String, String> result = new HashMap<>();
@@ -24,6 +31,10 @@ public class KakaoOauthController {
         return result;
     }
 
+    @Operation(summary = "로그인/회원가입 API", description = "회원 정보가 있을 경우 Login / 없을 경우 회원가입", tags = { "KakaoOauthController" })
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "OK")
+    })
     @PostMapping("/v1/api/login")
     public ResponseEntity<AuthTokens> loginKakao(@RequestBody KakaoLoginParams params) {
         return ResponseEntity.ok(oAuthLoginService.login(params));
